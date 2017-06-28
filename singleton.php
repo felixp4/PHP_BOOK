fefed3181d05676db87742ef31f33b6d16b39e6b
felix <felixp4@yandex.ru>
2017-06-28 15:07:55 +0300

    Testing pattern Singleton

--- singleton.php
+++ singleton.php
@@ -3,6 +3,17 @@
 final class Singleton
 {
     private static $instance = null;
+    private $bar = 0;
+
+    public function getBar()
+    {
+        return $this->bar;
+    }
+
+    public function incBar()
+    {
+        $this->bar++;
+    }
 
     public static function getInstance(): self
     {
@@ -13,20 +24,37 @@ final class Singleton
         return self::$instance;
     }
 
+    // защита от создания через конструктор
     private function __constructor()
     {
-        echo "Привет из конструктора !!! Инстанцироваться не получится ... ";
-        return;
+        // echo "Привет из конструктора !!! Инстанцироваться не получится ... ";
+        // return self::$instance;
+    }
+
+    // защита от клонирования
+    private function __clone()
+    {
+
+    }
+
+    // защита от сериализации
+    private function __wakeup()
+    {
+
     }
 }
 
 $inst = Singleton::getInstance();
 var_dump($inst);
+$inst->incBar();
+var_dump($inst->getBar());
 
 $inst1 = Singleton::getInstance();
 var_dump($inst1);
+$inst1->incBar();
+var_dump($inst1->getBar());
 
-// $inst_construct = new Singleton();
-// var_dump($inst_construct);
+$inst_construct = new Singleton();
+var_dump($inst_construct);
 
 
